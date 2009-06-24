ed02ff627967c917809c5ede866a16684295ea65
mala <github@ma.la>
2009-06-24 16:08:47 +0900

    initial commit

---
+++ lib/XML/Convert.pm
@@ -0,0 +1,33 @@
+package XML::Convert;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+XML::Convert -
+
+=head1 SYNOPSIS
+
+  use XML::Convert;
+
+=head1 DESCRIPTION
+
+XML::Convert is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
